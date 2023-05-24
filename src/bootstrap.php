86aa9c1a7a82c4f50f77a10825d76134056a8797
Amir <a.shadanfar.IT@gmail.com>
2023-05-24 13:26:52 +0300

    install phpdotenv to access environment variables

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -3,9 +3,14 @@
 declare(strict_types=1);
 
 use App\App;
+use Dotenv\Dotenv;
 
 require __DIR__ . '/config/path_constants.php';
 require ROOT_PATH . '/vendor/autoload.php';
+
+// load environment file
+$dotenv = Dotenv::createImmutable(ROOT_PATH);
+$dotenv->load();
 
 // initial container
 $container = require CONFIG_PATH . '/container.php';
