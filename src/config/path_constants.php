b085c1c8af1c3b8c5b820b5781e6d7195da0c25e
Amir <a.shadanfar.IT@gmail.com>
2024-08-12 00:26:49 +0200

    rename view directory to template

--- src/config/path_constants.php
+++ src/config/path_constants.php
@@ -6,6 +6,6 @@ const ROOT_PATH = __DIR__ . '/..';
 const APP_PATH = __DIR__ . '/../app';
 const PUBLIC_PATH = __DIR__ . '/../public';
 const CONFIG_PATH = __DIR__ . '/../config';
-const VIEW_PATH = __DIR__ . '/../view';
+const VIEW_PATH = __DIR__ . '/../templates';
 const STORAGE_PATH = __DIR__ . '/../storage';
 const MIGRATION_PATH = __DIR__ . '/../migrations';
